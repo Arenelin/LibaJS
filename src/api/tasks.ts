591659e98ae086030e93f368e6302383ea49078c
nakmaykin <nakmaykin@sfxdx.com>
2024-12-12 07:38:56 +0700

    fix: fixed url for getting tasks

--- src/api/tasks.ts
+++ src/api/tasks.ts
@@ -2,5 +2,5 @@ import {fetchInstance} from "./interceptor";
 import {TaskEntity} from "../Todolist.component";
 
 export const getTasks = async (todolistId: string): Promise<TaskEntity[]> => {
-    return fetchInstance(`/todo-lists${todolistId}/tasks`)
+    return fetchInstance(`/todo-lists/${todolistId}/tasks`)
 }
