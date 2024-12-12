cb223c747863116b5627058f948ea744fd5352fa
nakmaykin <nakmaykin@sfxdx.com>
2024-12-12 07:37:45 +0700

    feat: adds content type headers to the request

--- src/api/interceptor.ts
+++ src/api/interceptor.ts
@@ -1,4 +1,10 @@
-export const fetchInstance = async (url: string) => {
-    return fetch(`https://social-network.samuraijs.com/api/1.1/${url}`, {credentials: 'include'})
+export const fetchInstance = async (url: string, init?: RequestInit) => {
+    return fetch(`https://social-network.samuraijs.com/api/1.1${url}`, {
+        credentials: 'include',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        ...init
+    })
         .then(r => r.json());
 }
