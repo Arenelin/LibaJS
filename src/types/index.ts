a58317724578052669765e48fcbee3800a8c9753
nakmaykin <nakmaykin@sfxdx.com>
2024-12-21 07:23:24 +0700

    refactor: removes unused types

--- src/types/index.ts
+++ src/types/index.ts
@@ -26,10 +26,6 @@ export type LocalState<S> = {
     value: S
 }
 
-export type SetStateAction<S> = S | ((prevState: S) => S);
-
-export type Dispatch<A> = (value: A) => void;
-
 export type ComponentLiba = {
     refresh: () => void
     useObservable: <S>(initialState: LocalState<S>) => LocalState<S>
